cd68a4f9140f3454aaa9e7a059f4640fb8a1ea0b
Alberto Iong <albert0i@hotmail.com>
2024-08-05 15:05:41 +0800

    write more

---
+++ test/num.test.js
@@ -0,0 +1,17 @@
+
+test("adds 1 + 1 to equal 3", () => {
+    expect(1 + 1).toBe(2)
+})
+
+/*
+   JavaScript Testing with Jest – Crash Course
+   https://youtu.be/IPiUDhwnZxA
+
+   Getting `TypeError: jest.fn is not a function`
+   https://stackoverflow.com/questions/46086970/getting-typeerror-jest-fn-is-not-a-function
+
+   How can I test that a function has not been called?
+   https://stackoverflow.com/questions/24282390/how-can-i-test-that-a-function-has-not-been-called
+
+   npm test -t num
+*/
