2df5693d4f7a645d0f677fadda0ddd53f10ed935
Alberto Iong <albert0i@hotmail.com>
2024-08-07 15:31:09 +0800

    add delete test

---
+++ test/redis-delete.test.js
@@ -0,0 +1,30 @@
+import { redisClient, disconnect } from '../src/config/redisClient.js'
+
+afterAll( async () => await redisClient.ping() );
+afterAll( async () => await disconnect() );
+
+test("get 100,000 posts", async () => {
+  // Time matters... 
+  let response, i, j
+  for (let i=1; i <= 100000; i++) {
+      j = Math.floor(Math.random() * 1000000) + 1;
+      response = await redisClient.hgetall(`posts:${j}`)       
+      
+      response.id = parseInt(response.id, 10)
+      response.userId = parseInt(response.userId, 10)
+    }
+  }, 60 * 60 * 1000)
+
+test("get and del 100,000 posts", async () => {
+    // Time matters... 
+    let response, i, j
+    for (let i=1; i <= 100000; i++) {
+        j = Math.floor(Math.random() * 1000000) + 1;
+        response = await redisClient.hgetall(`posts:${j}`)       
+        
+        response.id = parseInt(response.id, 10)
+        response.userId = parseInt(response.userId, 10)
+
+        await redisClient.del(`posts:${j}`)
+      }
+    }, 60 * 60 * 1000)
---
+++ test/sql-delete.test.js
@@ -0,0 +1,46 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+afterAll( async () => await prisma.posts.count({}));
+afterAll( async () => await prisma.$disconnect() );
+
+test("select 100,000 posts (ORM)", async () => {
+    // Time matters... 
+    let response, i, j
+    for (let i=1; i <= 100000; i++) {
+        j = Math.floor(Math.random() * 1000000) + 1;
+        response = await prisma.posts.findUnique( { where: { id: j } } ) 
+    }
+}, 60 * 60 * 1000)
+
+test("select and delete 100,000 posts (ORM)", async () => {
+    // Time matters... 
+    let response, i, j
+    for (let i=1; i <= 100000; i++) {
+        j = Math.floor(Math.random() * 1000000) + 1;
+        response = await prisma.posts.findUnique( { where: { id: j } } ) 
+
+        await prisma.posts.delete( { where: { id: j } } )
+    }
+}, 60 * 60 * 1000)
+
+test.skip("select 100,000 posts (SQL)", async () => {
+    // Time matters... 
+    let response, i, j
+    for (let i=1; i <= 100000; i++) {
+        j = Math.floor(Math.random() * 1000000) + 1;
+        response = await prisma.$queryRaw`select * from posts where id=${j}`
+    }
+}, 60 * 60 * 1000)
+
+test.skip("select and delete 100,000 posts (SQL)", async () => {
+    // Time matters... 
+    let response, i, j
+    for (let i=1; i <= 100000; i++) {
+        j = Math.floor(Math.random() * 1000000) + 1;
+        response = await prisma.$queryRaw`select * from posts where id=${j}`
+
+        await prisma.$executeRaw`delete from posts where id=${j}`
+    }
+}, 60 * 60 * 1000)
